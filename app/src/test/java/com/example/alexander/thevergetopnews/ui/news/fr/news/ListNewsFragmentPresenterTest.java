a619814ba385267e0439cc41e78a3640ce96d8ba
Max <maxmmelnikov@gmail.com>
2018-12-03 22:30:13 +0200

    # network request test example

--- app/src/test/java/com/example/alexander/thevergetopnews/ui/news/fr/news/ListNewsFragmentPresenterTest.java
+++ app/src/test/java/com/example/alexander/thevergetopnews/ui/news/fr/news/ListNewsFragmentPresenterTest.java
@@ -1,58 +1,51 @@
 package com.example.alexander.thevergetopnews.ui.news.fr.news;
 
-
 import com.example.alexander.thevergetopnews.components.dto.News;
 import com.example.alexander.thevergetopnews.components.network.INetwork;
 import com.example.alexander.thevergetopnews.components.network.NetworkCallback;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 @RunWith(MockitoJUnitRunner.class)
 public class ListNewsFragmentPresenterTest {
-
-    private ListNewsFragmentPresenter presenter;
 
     private final static String TOP_NEWS = "Top";
     private final static String APPLE = "Apple";
     private final static String ANDROID = "Android";
 
     @Mock
-   private INetwork mockNetwork;
+    private INetwork network;
 
     @Mock
-   private ListNewsFragmentContract.IView mockView;
+    private ListNewsFragmentContract.IView view;
 
-    @Mock
-     private NetworkCallback mockListener;
+    private ListNewsFragmentPresenter presenter;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        presenter = new ListNewsFragmentPresenter(mockView, mockNetwork);
-        }
+        presenter = new ListNewsFragmentPresenter(view, network);
+    }
 
     @Test
     public void getDataSuccessTest() {
-        News data = new News();
-       presenter.getData(TOP_NEWS);
-     //  verify(mockNetwork).getTopNews(mockListener);
-        verify(mockView).showList(data);
+        final News data = new News();
+        final ArgumentCaptor<NetworkCallback<News>> capt = ArgumentCaptor.forClass(NetworkCallback.class);
 
-    }
+        presenter.getData(TOP_NEWS);
 
-    @After
-    public void tearDown() {
+        Mockito.verify(network, Mockito.times(1)).getTopNews(capt.capture());
+        capt.getValue().onFinished(data);
+        Mockito.verify(view, Mockito.times(1)).showList(data);
+
+        Mockito.verify(network, Mockito.never()).getAndroidNews(capt.capture());
+        Mockito.verify(network, Mockito.never()).getAppleNews(capt.capture());
     }
 }
